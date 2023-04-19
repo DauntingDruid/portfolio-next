ea3ab9a391793f8d4d3b505eb7f981fb44dfab96
Abhishek <abhishekroxtomar@gmail.com>
2023-04-19 23:41:33 +0530

    responsiveness fix

--- portfolio-next/src/pages/index.tsx
+++ portfolio-next/src/pages/index.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="bg-gray-950 flex min-h-screen flex-col items-center xs:p-12 md:p-24">
+    <main className="bg-gray-950 flex min-h-screen flex-col items-center p-5 md:p-24 xl:p-48">
       <Navbar />
       <AboutSection></AboutSection>
       <Bottomnav></Bottomnav>
