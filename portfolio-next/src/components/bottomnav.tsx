961a243666faa740bad5cfe274c5cbd6dee7ce44
Abhishek <abhishekroxtomar@gmail.com>
2023-04-19 20:33:01 +0530

    btm nav

--- portfolio-next/src/components/bottomnav.tsx
+++ portfolio-next/src/components/bottomnav.tsx
@@ -4,8 +4,12 @@ import React from 'react'
 const Bottomnav = () => {
   return (
     <div className='flex w-full'>
-      <img className='h-20 ease-in-out duration-150 hover:scale-125' src='../../assets/img/github.png' alt="github" />
-      <img className='h-20 ease-in-out duration-150 hover:scale-125' src='../../assets/img/linkedin.png' alt="linkedin" />
+      <a href="https://github.com/DauntingDruid" target="_blank">
+        <img className='h-20 ease-in-out duration-150 hover:scale-125' src='../../assets/img/github.png' alt="github" />
+      </a>
+      <a href="https://www.linkedin.com/in/abhishek-tomar-07" target="_blank">
+        <img className='h-20 ease-in-out duration-150 hover:scale-125' src='../../assets/img/linkedin.png' alt="linkedin" />
+      </a>
     </div>
   )
 }
