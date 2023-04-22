c158979a7e2c5bdcecb2513a83b507f0563118e3
Abhishek <abhishekroxtomar@gmail.com>
2023-04-22 17:02:17 +0530

    removed pointers on navbar

--- portfolio-next/src/components/navbar.tsx
+++ portfolio-next/src/components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className='container mx-auto'>
         <div className='flex justify-between items-center'>
         <div>
-            <a href="#" className='text-white ease-in-out duration-300 hover:line-through hover:text-lg hover:text-fuchsia-600'>Abhishek Tomar</a>
+            <a href="#" className='cursor-none text-white ease-in-out duration-300 hover:line-through hover:text-lg hover:text-fuchsia-600'>Abhishek Tomar</a>
         </div>
         <div className='flex items-center'>
             {/* <div className="mx-3">
@@ -19,8 +19,8 @@ const Navbar = () => {
             <div className="mx-3">
                 <a href="#">Contact</a>
             </div> */}
-            <div className='flex items-center ease-in-out duration-300 hover:scale-110 hover:rotate-6'>
-                <a href="mailto:abhishektomarwork@gmail.com" className='text-white hover:line-through hover:text-blue-400'>Lets connect?</a>
+            <div className='cursor-none flex items-center ease-in-out duration-300 hover:scale-110 hover:rotate-6'>
+                <a href="mailto:abhishektomarwork@gmail.com" className='cursor-none text-white hover:line-through hover:text-blue-400'>Lets connect?</a>
                 <Image width={40} height={40} src='/assets/img/paper.png' alt="send mail" />
             </div>
         </div>
