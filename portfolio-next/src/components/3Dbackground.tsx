90a4076e20a1edaafe613264a9a979edc9affac9
Abhishek <abhishekroxtomar@gmail.com>
2023-04-25 00:42:22 +0530

    added 3d object + text changes

--- portfolio-next/src/components/3Dbackground.tsx
+++ portfolio-next/src/components/3Dbackground.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+function Scene() {
+  const gltf = useLoader(GLTFLoader, './assets/rhetorician/scene.gltf')
+  return <primitive object={gltf.scene} />
+}
+
+
 
 
 const Background3D = () => {
   return (
-    <>
-    </>
+    <Scene />
   )
 }
 
--- portfolio-next/src/pages/index.tsx
+++ portfolio-next/src/pages/index.tsx
@@ -5,17 +5,34 @@ import Background3D from '@/components/3Dbackground'
 import AboutSection from '@/components/about'
 import Bottomnav from '@/components/bottomnav'
 import CursorCustom from '@/components/cursor'
+import { Canvas } from '@react-three/fiber'
+import { Center, Cloud, Float, Grid, Loader, OrbitControls, PerspectiveCamera, PivotControls, PresentationControls, Sparkles } from '@react-three/drei'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="bg-gray-950 flex min-h-screen flex-col items-center p-5 md:p-24 xl:p-24">
+    <main className="bg-gray-950 flex min-h-screen flex-col items-center ">
+      
+      <div className="absolute z-0 h-full w-full">
+        <Canvas> 
+          <OrbitControls enableZoom={false} ></OrbitControls>
+          <PerspectiveCamera zoom={2} makeDefault position={[0, 5, 15]} />
+          <Center>
+            <Float speed={2.5} >
+              <Sparkles count={50} size={2.5} noise={1.5}></Sparkles>
+              <Background3D></Background3D>
+            </Float>
+          </Center>  
+        </Canvas>
+        <Loader />
+      </div>
+      <div className="z-10 md:p-24 xl:p-24">
       <CursorCustom></CursorCustom>
       <Navbar />
       <AboutSection></AboutSection>
       <Bottomnav></Bottomnav>
-      <Background3D></Background3D>
+      </div>
     </main>
   )
 }
