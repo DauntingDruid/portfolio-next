6678669ed9dc7f096cc5d693a1d768b6add74bee
Abhishek <abhishekroxtomar@gmail.com>
2023-04-30 22:46:15 +0530

    loading screen

---
+++ portfolio-next/src/components/loadingScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingScreen = () => {
+  return (
+    <div className='flex w-full h-full justify-center content-center'>
+
+        
+
+    </div>
+  )
+}
+
+export default LoadingScreen
