ee099a91889b4cb61a3deba731d04838f54c38b4
Abhishek <abhishekroxtomar@gmail.com>
2023-05-01 00:22:59 +0530

    effects

--- portfolio-next/src/components/bottomnav.tsx
+++ portfolio-next/src/components/bottomnav.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 const Bottomnav = () => {
   return (
     <div className='xs:flex-row sm:flex xs:w-full justify-evenly  lg:w-9/12'>
-      <a className='cursor-none flex justify-center' download="AbhishekTomar_resume.pdf" href="../../assets/file/AbhishekTomar_resume.pdf">
+      <a className='cursor-none hover:animate-pulse flex justify-center' download="AbhishekTomar_resume.pdf" href="../../assets/file/AbhishekTomar_resume.pdf">
         <Image width={80} height={80} src='/assets/img/applicant.png' className='ease-in-out duration-150 hover:scale-125'  alt="resume" />
       </a>
-      <a className='cursor-none flex justify-center' href="https://github.com/DauntingDruid" target="_blank">
+      <a className='cursor-none hover:animate-pulse flex justify-center' href="https://github.com/DauntingDruid" target="_blank">
         <Image width={80} height={80} src='/assets/img/github.png' className='ease-in-out duration-150 hover:scale-125'  alt="github" />
       </a>
-      <a className='cursor-none flex justify-center' href="https://www.linkedin.com/in/abhishek-tomar-07" target="_blank">
+      <a className='cursor-none hover:animate-pulse flex justify-center' href="https://www.linkedin.com/in/abhishek-tomar-07" target="_blank">
         <Image width={80} height={80} src='/assets/img/linkedin.png' className='ease-in-out duration-150 hover:scale-125'  alt="linkedin" />
       </a>
     </div>
